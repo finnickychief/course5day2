a3bff5827a34ea1a85e6eec2bdcd0a8430e8daed
Jeffery Patton <jefferypatton@Jeff-Pattons-MacBook-Pro.local>
2018-09-05 11:21:45 -0400

    first commit

---
+++ index1.js
@@ -0,0 +1,34 @@
+/*
+  DOM Manipulation with Javascript:
+  
+
+  getElementById <-- Works off the ID and the ID only
+  querySelector <-- Works off the CSS rules for selecting items
+
+  Example:
+    const responseDiv = document.querySelector('#responseDiv');
+
+  After you have the item:
+    Changing the text within: innerHTML, innerText, value
+    Change the attributes using properties like className, classList, and some have href/src attributes
+    Changing the style: .style properties, as well as cssText
+    Add events/interactivity to our dom elements using addEventListener
+    Add children to the item using appendChild
+
+  Create elements:
+    document.createElement('Element Name')
+    document.createElement('div')
+    document.createElement('img')
+    document.createElement('a')
+
+  Form validation
+    test that your data is valid before sending it to a backend/API
+
+
+
+
+
+
+
+
+*/
