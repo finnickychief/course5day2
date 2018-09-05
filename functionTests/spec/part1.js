981f265fe0bba1bb4c0536288151d688f7dc859b
Phlebsy <Jtpatton92@gmail.com>
2018-09-05 14:03:44 -0400

    Add files via upload

---
+++ functionTests/spec/part1.js
@@ -0,0 +1,128 @@
+(function() {
+  'use strict';
+  describe('Javascript Exercises', function() {
+    describe('vowels', function() {
+      it('should expect vowels(`aeiou`) to be 5', function() {
+        expect(vowels('aeiou')).to.equal(5);
+      });
+      it('should expect vowels(`AEIOU`) to be 5', function() {
+        expect(vowels(`AEIOU`)).to.equal(5);
+      });
+      it('should expect vowels(`abcdefghijklmnopqrstuvwxyz`) to be 5', function() {
+        expect(vowels(`abcdefghijklmnopqrstuvwxyz`)).to.equal(5);
+      });
+      it('should expect vowels(`bcdfghjkl`) to be 0', function() {
+        expect(vowels(`bcdfghjkl`)).to.equal(0);
+      });
+      it('should expect vowels(`aabbccddee`) to be 4', function() {
+        expect(vowels(`aabbccddee`)).to.equal(4);
+      });
+    });
+
+    describe('capitalize', function() {
+      it('should expect capitalize(`hi there, how is it going?`) to be `Hi There, How Is It Going?`', function() {
+        expect(`hi there, how is it going?`).to.equal(
+          `Hi There, How Is It Going?`
+        );
+      });
+      it('should expect capitalize(`i love breakfast at bill millers bbq`) to be `I Love Breakfast At Bill Miller Bbq`', function() {
+        expect(`i love breakfast at bill millers bbq`).to.equal(
+          `I Love Breakfast At Bill Miller Bbq`
+        );
+      });
+      it('should expect capitalize(`this is a Sentence.`) to be `This Is A Sentence.`', function() {
+        expect(`this is a Sentence.`).to.equal(`This Is A Sentence.`);
+      });
+    });
+    describe('anagrams', function() {
+      it('should expect anagrams(`hello`, `llohe`) to be true', function() {
+        expect(anagrams(`hello`, `llohe`)).to.equal(true);
+      });
+      it('should expect anagrams(`Whoa! Hi!`, `Hi! Whoa!`) to be true', function() {
+        expect(anagrams(`Whoa! Hi!`, `Hi! Whoa!`)).to.equal(true);
+      });
+      it('should expect anagrams(`One One`, `Two two two`) to be false', function() {
+        expect(anagrams(`One One`, `Two two two`)).to.equal(false);
+      });
+      it('should expect anagrams(`One one`, `One one c`) to be false', function() {
+        expect(anagrams(`One one`, `One one c`)).to.equal(false);
+      });
+      it('should expect anagrams(`A tree, a life, a bench`, `A tree, a fence, a yard`) to be false', function() {
+        expect(
+          anagrams(`A tree, a life, a bench`, `A tree, a fence, a yard`)
+        ).to.equal(false);
+      });
+    });
+    describe('chunk', function() {
+      it('should expect chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2) to be [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]', function() {
+        expect(chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2)).to.deep.equal([
+          [1, 2],
+          [3, 4],
+          [5, 6],
+          [7, 8],
+          [9, 10]
+        ]);
+      });
+      it('should expect chunk([1,2,3], 1) to be [[1], [2], [3]]', function() {
+        expect(chunk([1, 2, 3], 1)).to.deep.equal([[1], [2], [3]]);
+      });
+      it('should expect chunk([1,2,3,4,5], 3) to be [[1,2,3], [4,5]]', function() {
+        expect(chunk([1, 2, 3, 4, 5], 3)).to.deep.equal([[1, 2, 3], [4, 5]]);
+      });
+      it('should expect chunk([1,2,3,4,5,6,7,8,9,10,11,12,13], 5) to be [[1,2,3,4,5], [6,7,8,9,10], [11,12,13]]', function() {
+        expect(
+          chunk([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13], 5)
+        ).to.deep.equal([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13]]);
+      });
+    });
+    describe('palindrome', function() {
+      it('should expect palindrome(``) to be true', function() {
+        expect(palindrome(``)).to.equal(true);
+      });
+      it('should expect palindrome(`aba`) to be true', function() {
+        expect(palindrome(`aba`)).to.equal(true);
+      });
+      it('should expect palindrome(` aba`) to be false', function() {
+        expect(palindrome(` aba`)).to.equal(false);
+      });
+      it('should expect palindrome(`aba `) to be false', function() {
+        expect(palindrome(`aba `)).to.equal(false);
+      });
+      it('should expect palindrome(`greetings`) to be false', function() {
+        expect(palindrome(`greetings`)).to.equal(false);
+      });
+      it('should expect palindrome(`1000000001`) to be true', function() {
+        expect(palindrome(`1000000001`)).to.equal(true);
+      });
+      it('should expect palindrome(`pennep`) to be true', function() {
+        expect(palindrome(`pennep`)).to.equal(true);
+      });
+    });
+    describe('reverseInt', function() {
+      it('should expect reverseInt() to be ', function() {
+        expect(reverseInt(5)).to.equal(5);
+      });
+      it('should expect reverseInt(10) to be 1', function() {
+        expect(reverseInt(10)).to.equal(1);
+      });
+      it('should expect reverseInt(98) to be 89', function() {
+        expect(reverseInt(98)).to.equal(89);
+      });
+      it('should expect reverseInt(52) to be 25', function() {
+        expect(reverseInt(52)).to.equal(25);
+      });
+      it('should expect reverseInt(78) to be 87', function() {
+        expect(reverseInt(78)).to.equal(87);
+      });
+      it('should expect reverseInt(12) to be 21', function() {
+        expect(reverseInt(12)).to.equal(21);
+      });
+      it('should expect reverseInt(11) to be 11', function() {
+        expect(reverseInt(17)).to.equal(71);
+      });
+      it('should expect reverseInt(700) to be 7', function() {
+        expect(reverseInt(700)).to.equal(7);
+      });
+    });
+  });
+})();
---
+++ functionTests/src/javascript.js
@@ -0,0 +1,68 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels() {}
+
+// --- Directions
+// Write a function that accepts a string.  The function should
+// capitalize the first letter of each word in the string then
+// return the capitalized string.
+// --- Examples
+//   capitalize('a short sentence') --> 'A Short Sentence'
+//   capitalize('a lazy fox') --> 'A Lazy Fox'
+//   capitalize('look, it is working!') --> 'Look, It Is Working!'
+
+function capitalize() {}
+
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams() {}
+
+// --- Directions
+// Given an array and chunk size, divide the array into many subarrays
+// where each subarray is of length size
+// --- Examples
+// chunk([1, 2, 3, 4], 2) --> [[ 1, 2], [3, 4]]
+// chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
+// chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
+// chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+
+function chunk() {}
+
+// --- Directions
+// Given a string, return true if the string is a palindrome
+// or false if it is not.  Palindromes are strings that
+// form the same word if it is reversed. *Do* include spaces
+// and punctuation in determining if the string is a palindrome.
+// --- Examples:
+//   palindrome("abba") === true
+//   palindrome("abcdefg") === false
+
+function palindrome() {}
+
+// --- Directions
+// Given an integer, return an integer that is the reverse
+// ordering of numbers.
+// --- Examples
+//   reverseInt(15) === 51
+//   reverseInt(981) === 189
+//   reverseInt(500) === 5
+//   reverseInt(-15) === -51
+//   reverseInt(-90) === -9
+
+function reverseInt(n) {}
