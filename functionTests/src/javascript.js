fe4ba00b1b754fd901a048f7d06cadd69b91bbde
finnickychief <41382984+finnickychief@users.noreply.github.com>
2018-09-06 12:21:49 -0400

    Add files via upload

--- functionTests/src/javascript.js
+++ functionTests/src/javascript.js
@@ -90,30 +90,140 @@ function capitalize(sentence) {
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams() {
-  // Do sort method then charmap method
+// Cheatsheet for regular expressions: https://www.rexegg.com/regex-quickstart.html
+// Remove all dollar signs from a string:
+let exampleString = 'This$ has$$$$ too $many$ dollarsigns';
+// the replace method takes in a 'mask', and a replacement value for anything that is matched by the mask.
+// The double /'s are used to indicate that it is a regular expression, and the g tells the replace method to search the entire string. If you only want to look for one value, you can place it within the //'s by itself like this:
 
-  // Cheasheet for regular expressions: https://www.rexegg.com/regex-quickstart.html
-  // Remove all dollar signs from a string:
-  let exampleString = 'This$ has$$$$ too $many$ dollarsigns';
-  // the replace method takes in a 'mask', and a replacement value for anything that is matched by the mask.
-  // The double /'s are used to indicate that it is a regular expression, and the g tells the replace method to search the entire string. If you only want to look for one value, you can place it within the //'s by itself like this:
+// Remove spaces from the string
+exampleString = exampleString.replace(/ /g, '');
 
-  // Remove spaces from the string
-  exampleString = exampleString.replace(/ /g, '');
+// Remove $ signs:(you must escape the $ because it is a reserved character that has another meaning in regular expression syntax)
+exampleString = exampleString.replace(/\$/g, '');
 
-  // Remove $ signs:(you must escape the $ because it is a reserved character that has another meaning in regular expression syntax)
-  exampleString = exampleString.replace(/\$/g, '');
+// With regular expressions you can look for more than one value to replace by using the brackets - []'s. List out all of the items you want to target within them, and it will search for all of them at once.
 
-  // With regular expressions you can look for more than one value to replace by using the brackets - []'s. List out all of the items you want to target within them, and it will search for all of them at once.
+// So to replace both the $ signs and the spaces use the following:
+exampleString = exampleString.replace(/[ \$]/g, '');
 
-  // So to replace both the $ signs and the spaces use the following:
-  exampleString = exampleString.replace(/[ \$]/g, '');
+// Replace all non-alphanumeric characters:
+exampleString = exampleString.replace(/\W/g, '');
 
-  // Replace all non-alphanumeric characters:
-  exampleString = exampleString.replace(/\W/g, '');
+// anagrams with the sort method
+function anagramsSortMethod(inString1, inString2) {
+  inString1 = inString1.replace(/\W/g, ''); // Strip all non-alphanumeric characters
+  inString2 = inString2.replace(/\W/g, '');
 
-  //replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+  // If the remaining strings without punctuation don't have the same amount of  characters overall, they cannot be anagrams
+  if (inString1.length !== inString2.length) {
+    return false;
+  }
+
+  /*
+    railsafety
+    fairytales
+
+    aaefilrsty
+    aaefilrsty
+
+    r: 1
+    a: 2
+    i: 1
+    l: 1
+    s: 1
+    f: 1
+    e: 1
+    t: 1
+    y: 1
+
+    f: 1
+    a: 2
+    i: 1
+    r: 1
+    y: 1
+    t: 1
+    l: 1
+    e: 1
+    s: 1
+
+    Hithere
+    Byethere
+
+    eehHirt <-- First string sorted
+    Beeehrty <-- second string sorted
+  */
+
+  // console.log(inString1);
+  // console.log(inString2);
+
+  let stringAry1 = inString1.split('');
+  let stringAry2 = inString2.split('');
+
+  // console.log(stringAry1);
+  // console.log(stringAry2);
+
+  stringAry1.sort();
+  stringAry2.sort();
+
+  // console.log(stringAry1);
+  // console.log(stringAry2);
+
+  for (let i = 0; i < stringAry1.length; i++) {
+    if (stringAry1[i] !== stringAry2[i]) {
+      // If any of our characters from the first string do not match the character at the same position in the second string, we return false because they are not anagrams
+      return false;
+    }
+  }
+
+  return true; // If they all pass, return true
+}
+
+function anagrams(inString1, inString2) {
+  // Clean out punctuation
+  inString1 = inString1.replace(/\W/g, '');
+  inString2 = inString2.replace(/\W/g, '');
+
+  if (inString1.length !== inString2.length) {
+    return false;
+  }
+
+  // Declare character map
+  const charMap1 = {};
+  const charMap2 = {};
+
+  // Fill in character map
+  for (let i = 0; i < inString1.length; i++) {
+    // Logic for filling out map:
+    // If the current character already exists in the map: increment the value tied to it
+    if (charMap1[inString1[i]]) {
+      charMap1[inString1[i]]++;
+    } else {
+      // Otherwise, put the character in the map with a value of 1
+      charMap1[inString1[i]] = 1;
+    }
+    if (charMap2[inString2[i]]) {
+      charMap2[inString2[i]]++;
+    } else {
+      charMap2[inString2[i]] = 1;
+    }
+  }
+
+  let letters = Object.keys(charMap1); // Get all of the keys off the first charMap
+  // We don't need the keys of the second one because we know what they SHOULD be if they are anagrams based on the first map
+
+  // Loop through all the keys
+  for (let i = 0; i < letters.length; i++) {
+    // Check if the number of occurences in each map is the same for a given key
+    let currentLetter = letters[i];
+
+    if (charMap1[currentLetter] !== charMap2[currentLetter]) {
+      return false; // If they don't occur the same amount of times, return false
+    }
+  }
+
+  // If all letters occur the same number of times, return true
+  return true;
 }
 
 // --- Directions
